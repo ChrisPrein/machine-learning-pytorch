7a021f0c0c848ce27d03b533caa7c4651d35e58d
ChrisPrein <79170851+ChrisPrein@users.noreply.github.com>
2023-01-22 17:56:57 +0100

    fixes scheduler checkpoint creation

--- src/machine_learning_pytorch/training/plugins/pytorch_scheduler_plugin.py
+++ src/machine_learning_pytorch/training/plugins/pytorch_scheduler_plugin.py
@@ -25,7 +25,20 @@ class PyTorchSchedulerPlugin(PreLoop[TInput, TTarget, TModel, TTrainer], PostEpo
         self.event_loop: asyncio.AbstractEventLoop = event_loop if event_loop != None else asyncio.get_event_loop()
 
     def pre_loop(self, logger: Logger, training_context: TrainingContext[TInput, TTarget, TModel, TTrainer]):
-        self.scheduler = self.event_loop.run_until_complete(self.repository.get(LATEST_SCHEDULER_NAME))
+        logger.info('Loading scheduler checkpoint...')
+        loaded_scheduler = self.event_loop.run_until_complete(self.repository.get(LATEST_SCHEDULER_NAME))
+
+        if loaded_scheduler != None:
+            self.scheduler = loaded_scheduler
+            logger.info('Scheduler checkpoint loaded!')
+        else:
+            logger.info('No scheduler checkpoint found!')
 
     def post_epoch(self, logger: Logger, training_context: TrainingContext[TInput, TTarget, TModel, TTrainer]):
+        logger.info('Performing scheduler step...')
         self.scheduler.step()
+        logger.info('Scheduler step performed!')
+
+        logger.info('Creating scheduler checkpoint...')
+        self.event_loop.create_task(self.repository.save(self.scheduler, LATEST_SCHEDULER_NAME))
+        logger.info('Scheduler checkpoint created!')
